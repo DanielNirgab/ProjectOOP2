bd88c48a9581abf97c718f6a44dd757e26be17ab
DanielNigrab <bgrln347@gmail.com>
2022-05-23 22:23:11 +0300

    Code

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -4,20 +4,17 @@ public class ServiceStation implements Service {
         wheelsService(car);
         engineCheck(car);
     }
-
     @Override
     public void transportService(Bicycle bicycle) {
         wheelsService(bicycle);
 
     }
-
     @Override
     public void transportService(Truck truck) {
         wheelsService(truck);
         engineCheck(truck);
         truck.checkTrailer();
     }
-
     private void wheelsService(Transport transport) {
         if (transport != null) {
             System.out.println("Обслуживаем " + transport.getModelName());
@@ -26,36 +23,10 @@ public class ServiceStation implements Service {
             }
         }
     }
-
     private void engineCheck(Transport transport) {
         if (transport != null) {
             transport.checkEngine();
         }
     }
-
-
-        //    public void check(Car car, Bicycle bicycle, Truck truck) {
-//        if (car != null) {
-//            System.out.println("Обслуживаем " + car.modelName);
-//            for (int i = 0; i < car.wheelsCount; i++) {
-//                car.updateTyre();
-//            }
-//            car.checkEngine();
-//        } else if (truck != null) {
-//            System.out.println("Обслуживаем " + truck.modelName);
-//            for (int i = 0; i < truck.wheelsCount; i++) {
-//                truck.updateTyre();
-//            }
-//            truck.checkEngine();
-//            truck.checkTrailer();
-//        } else if (bicycle != null) {
-//            System.out.println("Обслуживаем " + bicycle.modelName);
-//            for (int i = 0; i < bicycle.wheelsCount; i++) {
-//                bicycle.updateTyre();
-//            }
-//        }
-//    }
-
-
 
 }
